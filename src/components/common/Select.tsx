598d9c9828ef1ddb8accd453894aacd60cfae59f
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-08-13 20:18:53 +1000

    feat(*): Create base Select component
    - Create base component for user selectable data

---
+++ src/components/common/Select.tsx
@@ -0,0 +1,19 @@
+import InputLabel from "./InputLabel";
+import "./Select.sass";
+
+interface SelectProps {
+  onChange: React.ChangeEventHandler<HTMLSelectElement>;
+  children: React.ReactNode;
+  value: string;
+  label: string;
+}
+
+export default function Select(props: SelectProps): JSX.Element {
+  return (
+    <InputLabel label={props.label}>
+      <select className="select" onChange={props.onChange} value={props.value}>
+        {props.children};
+      </select>
+    </InputLabel>
+  );
+}
